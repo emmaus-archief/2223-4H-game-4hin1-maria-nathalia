e3191fd8c66c1a0b570404247a8cadc907f57de7
hedgehogmo <112248649+hedgehogmo@users.noreply.github.com>
2023-06-01 15:12:54 +0000

    fixed syntax error, uitleg2 screen (win screen)

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 ///<reference path="p5.global-mode.d.ts" />
-"use strict"
+"use strict";
 
 function preload() {
   img = loadImage("sillycat.png");
-  img2 = loadImage ("gosig.png");
-  img3 = loadImage ("bowl.png");
+  img2 = loadImage("gosig.png");
+  img3 = loadImage("bowl.png");
 }
 
-//global variables
+// Global variables
 const SPELEN = 1;
 const GAMEOVER = 2;
 const UITLEG = 3;
@@ -29,143 +29,140 @@ var mouseIsPressedBefore = false;
 var mouseX = 0;
 var mouseY = 0;
 
-var timer = 500; // 10 seconde
+var timer = 500; // 10 seconden
 
-
-//functions
+// Functions
 /**
  * Updatet globale variabelen met posities van speler, vijanden en kogels
  */
-var beweegAlles = function() {
-  background ("green");
+var beweegAlles = function () {
+  background("green");
   // player
-  if (keyIsDown(65)){
-    spelerX = spelerX -2;
-  };
-  if (keyIsDown(68)){
-    spelerX = spelerX +2;
-  };
-  if (keyIsDown(87)){
-    spelerY = spelerY -2;
-  };
-  if (keyIsDown(83)){
-    spelerY = spelerY +2;
-  };
-  
-    //sprint when shift is down
-  if (keyIsDown(65)&&keyIsDown(16)){
-    spelerX = spelerX -4;
-  };
-  if (keyIsDown(68)&&keyIsDown(16)){
-    spelerX = spelerX +4;
-  };
-  if (keyIsDown(87)&&keyIsDown(16)){
-    spelerY = spelerY -4;
-  };
-  if (keyIsDown(83)&&keyIsDown(16)){
-    spelerY = spelerY +4;
-  };
-    
-  //borders
+  if (keyIsDown(65)) {
+    spelerX = spelerX - 2;
+  }
+  if (keyIsDown(68)) {
+    spelerX = spelerX + 2;
+  }
+  if (keyIsDown(87)) {
+    spelerY = spelerY - 2;
+  }
+  if (keyIsDown(83)) {
+    spelerY = spelerY + 2;
+  }
+
+  // sprint when shift is down
+  if (keyIsDown(65) && keyIsDown(16)) {
+    spelerX = spelerX - 4;
+  }
+  if (keyIsDown(68) && keyIsDown(16)) {
+    spelerX = spelerX + 4;
+  }
+  if (keyIsDown(87) && keyIsDown(16)) {
+    spelerY = spelerY - 4;
+  }
+  if (keyIsDown(83) && keyIsDown(16)) {
+    spelerY = spelerY + 4;
+  }
+
+  // borders
   if (spelerY > 1080) {
-      spelerY = 1080;
-  };
+    spelerY = 1080;
+  }
   if (spelerY < 470) {
     spelerY = 470;
-  };
-  if (spelerX > 2490) { 
-    spelerX = 2490; 
-  };
+  }
+  if (spelerX > 2490) {
+    spelerX = 2490;
+  }
 
   if (spelerX < 60) {
-    spelerX = 60; 
-  };
+    spelerX = 60;
+  }
 
-   // timer aanpassen
-    timer = timer - 1;
-  
-// clicks op recept onderdelen
- if (mouseIsPressed && !mouseIsPressedBefore &&
-     mouseX - cooking1X < 50 &&  mouseX - cooking1X > -50 &&
-     mouseY - cooking1Y < 50 && mouseY - cooking1Y > -50) {
-         cooking1Clicks +=1; 
+  // timer aanpassen
+  timer = timer - 1;
+
+  // clicks op recept onderdelen
+  if (
+    mouseIsPressed &&
+    !mouseIsPressedBefore &&
+    mouseX - cooking1X < 50 &&
+    mouseX - cooking1X > -50 &&
+    mouseY - cooking1Y < 50 &&
+    mouseY - cooking1Y > -50
+  ) {
+    cooking1Clicks += 1;
   }
 
   mouseIsPressedBefore = mouseIsPressed;
 };
 
-
 var countClicks = function () {
   if (cooking1Clicks === 35) {
     console.log("enoughclicks");
   }
-}
+};
 
-  
-//variables of objects and player that are drawn
+// Variables of objects and player that are drawn
 var kitchenCounterUp = function () {
-  fill ("blue"); //backdrop
-  rect (0,0,2560,250);
-  
+  fill("blue"); // backdrop
+  rect(0, 0, 2560, 250);
+
   fill("red");
-  rect (0, 250, 2560, 300);
+  rect(0, 250, 2560, 300);
 
-  fill("orange");  //upper counter top
-  rect (0, 250, 2560, 150);
-}
+  fill("orange"); // upper counter top
+  rect(0, 250, 2560, 150);
+};
 
-  var drawPlayer = function () {
+var drawPlayer = function () {
   fill("white");
-  rect(spelerX - 70, spelerY - 70, 140, 140); //body
-  ellipse(spelerX, spelerY - 100, 150, 150);  //head
+  rect(spelerX - 70, spelerY - 70, 140, 140); // body
+  ellipse(spelerX, spelerY - 100, 150, 150); // head
   fill("black");
-  ellipse(spelerX, spelerY, 20, 20);  //centerpoint
-}
+  ellipse(spelerX, spelerY, 20, 20); // centerpoint
+};
 
 var kitchenCounterDown = function () {
-  fill ("red");
-  rect (0, 1100, 2560, 300);  //down right counter
-  rect (2100,1100, 800, 300);
-  
-}
+  fill("red");
+  rect(0, 1100, 2560, 300); // down right counter
+  rect(2100, 1100, 800, 300);
+};
 
 var drawKitchenUtensil = function () {
-  fill (218, 66, 245);
-  ellipse (cooking1X, cooking1Y, 250,200);
+  fill(218, 66, 245);
+  ellipse(cooking1X, cooking1Y, 250, 200);
 
-  fill (0,0,0);
+  fill(0, 0, 0);
   textSize(100);
-  text (cooking1Clicks, 200,200);
-}
+  text(cooking1Clicks, 200, 200);
+};
 
+// Draws everything
+var tekenAlles = function () {
+  text("timer:" + timer, 600, 100);
+};
 
-//draws everything
-var tekenAlles = function() {
-  text("timer:"+timer, 600, 100);
-  }
-
-//scene checker/scene changes
-var checkGameOver = function() {
-  // check of HP 0 is , of tijd op is, of ...
+// Scene checker/scene changes
+var checkGameOver = function () {
+  // Check of HP 0 is, of tijd op is, of ...
   if (timer <= 0 && cooking1Clicks < 35) {
     return true;
   }
 
-  if (timer <= 0 && cooking1Clicks > 35)
-    spelStatus = UITLEG2;
+  if (timer <= 0 && cooking1Clicks > 35) spelStatus = UITLEG2;
 };
 
-
-
-// setup() en draw() functies / hoofdprogramma
-// setup, p5 library
+// setup() and draw() functions / hoofdprogramma
+// Setup, p5 library
 function setup() {
-  //canvas
+  // Canvas
   createCanvas(2560, 1350);
-  background('green');
+  background("green");
 }
 
-// draw function, after setup
+// Draw function, after setup
 function draw() {
   if (spelStatus === SPELEN) {
     beweegAlles();
@@ -178,18 +175,17 @@ function draw() {
     if (checkGameOver()) {
       spelStatus = GAMEOVER;
     }
-    console.log ("play");
+    console.log("play");
   }
   if (spelStatus === GAMEOVER) {
-    console.log ("gameover");
-      image(img2, 0, 0);
+    console.log("gameover");
+    image(img2, 0, 0);
     if (keyIsDown(32)) {
-      spelStatus = UITLEG; 
+      spelStatus = UITLEG;
     }
-    
   }
   if (spelStatus === UITLEG) {
-  console.log ("uitleg");
+    console.log("uitleg");
     image(img, 0, 0, 2560, 1280);
     if (keyIsDown(13)) {
       spelerX = 600;
@@ -197,9 +193,10 @@ function draw() {
       timer = 500;
       spelStatus = SPELEN;
     }
+  }
 
   if (spelStatus === UITLEG2) {
-  console.log ("uitleg2");
+    console.log("uitleg2");
     image(img3, 0, 0, 2560, 1280);
     if (keyIsDown(13)) {
       spelerX = 600;
@@ -208,5 +205,5 @@ function draw() {
       spelStatus = SPELEN;
     }
   }
-};
+}
 
